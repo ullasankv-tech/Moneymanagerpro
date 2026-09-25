8fdbecc579510432ee43d545f1e2bcc8df05e5fb
Ullasan KV <ullasankv@gmail.com>
2026-09-25 10:23:56 +0530

    Remove specific icon files from ASSETS array

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ const ASSETS = [
   BASE,
   BASE + 'index.html',
   BASE + 'manifest.json',
-  BASE + 'resources/icon-192.png',
-  BASE + 'resources/icon-512.png'
+  BASE + 'resources/icon.png',
+  BASE + 'resources/icon.png'
 ];
 
 self.addEventListener('install', event => {
